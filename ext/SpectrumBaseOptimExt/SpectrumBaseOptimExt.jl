51f6326ccee68432f670fc2e2b96586ca11d0366
Bon Leif Amalla <bonleifamalla@gmail.com>
2024-04-07 15:02:05 +0900

    Make an extension for shape fitting

---
+++ ext/SpectrumBaseOptimExt/SpectrumBaseOptimExt.jl
@@ -0,0 +1,15 @@
+# Optimization extension for SpectrumBase
+
+module SpectrumBaseOptimExt
+
+using SpectrumBase
+using StructArrays, Optim
+
+# define the fitting problem
+# make a solution struct for keeping the solution and
+# define functions for showing the fitting statistics of the solution
+# important ones are residues, sum square error, degrees of freedom, mse (rmsd)
+
+# define the solve function
+
+end
