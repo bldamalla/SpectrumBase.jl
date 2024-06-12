570fea209c743536ff634215ce40b6c41f1ab2b4
Bon Leif Amalla <bonleifamalla@gmail.com>
2024-06-12 09:43:01 +0900

    Make extensions work
    Also some changes to the calculation of objective

--- ext/SpectrumBaseOptimExt/SpectrumBaseOptimExt.jl
+++ ext/SpectrumBaseOptimExt/SpectrumBaseOptimExt.jl
@@ -2,63 +2,7 @@
 
 module SpectrumBaseOptimExt
 
-using SpectrumBase
-using StructArrays, Optim
-
-export FittingProblem
-export build_objective, solve
-
-# define the fitting problem
-# make a solution struct for keeping the solution and
-# define functions for showing the fitting statistics of the solution
-# important ones are residues, sum square error, degrees of freedom, mse (rmsd)
-
-"""
-    FittingProblem
-
-Defines a problem for subsequent fitting using a solve function.
-"""
-struct FittingProblem{LS<:LineShape,iP,xT,yT}
-    init_params::iP
-    xdata::xT
-    ydata::yT
-end
-function _evaluator(lineshape, params)
-    npars = SpectrumBase._nparams(lineshape)
-    pars_reshaped = reshape(params, npars, :)
-    structvec = StructArray{lineshape}(pars_reshaped; dims=1)
-    return function _tp(x)
-        return sum(structvec) do shape
-            evaluate(x, shape)
-        end
-    end
-end
-
-function build_objective(prob::FittingProblem{LS}) where LS
-    (; xdata, ydata) = prob
-    return function _obj(params)
-        # get y estimates from xdata
-        # get the sum of squares of residuals
-        evtor_ = _evaluator(LS, params)
-        return sum(zip(xdata, ydata)) do (x, y)
-            Δ = y - evtor_(x, params)
-            abs2(Δ)
-        end
-    end
-end
-
-## TODO: define the gradient for least squares objective
-
-"""
-    FittingSolution
-
-Represents a solution to a `FittingProblem`. Points to the original 
-`FittingProblem` solved along with the solution object from `Optim`.
-"""
-struct FittingSolution{fP<:FittingProblem,sT}
-    problem::fP
-    optimized::sT
-end
+using SpectrumBase, Optim
 
 # define the solve function
 """
@@ -66,10 +10,10 @@ end
 
 Solve a `FittingProblem` prob and return its optimized solution.
 """
-function solve(prob::FittingProblem)
+function SpectrumBase.solve(prob::FittingProblem)
     initparams = prob.init_params
     objfunc = build_objective(prob)
-    soln = optimize(objfunc, initparams, LBFGS(); autodiff=:forward)
+    soln = optimize(objfunc, initparams, LBFGS(), autodiff=:forward)
     return FittingSolution(prob, soln)
 end
 Optim.optimize(prob::FittingProblem) = solve(prob)
--- src/lineshapes.jl
+++ src/lineshapes.jl
@@ -21,13 +21,14 @@ function lscenter end
 function scale end
 
 _nparams(::T) where T<:LineShape = _nparams(T)
+function _lsparenttype(::LineShape) end
 
 struct Gaussian{xT,yT} <: LineShape{xT,yT}
     μ::xT
     σ::xT
     h::yT
     function Gaussian(μ::xT, σ::xT, h::yT) where {xT,yT}
-        σ > 0 || throw(ArgumentError("scale ``σ`` should be positive"))
+        #σ > 0 || throw(ArgumentError("scale ``σ`` should be positive"))
         return new{xT,yT}(μ,σ,h)
     end
 end
@@ -39,13 +40,14 @@ function evaluate(gs::Gaussian, x)
     expd = exp(scaled^2 * -1//2)
     return expd * gs.h / gs.σ
 end
+_lsparenttype(::Type{<:Gaussian}) = Gaussian
 
 struct Cauchy{xT,yT} <: LineShape{xT,yT}
     x₀::xT
     γ::xT
     h::yT
     function Cauchy(x₀::xT, γ::xT, h::yT) where {xT,yT}
-        γ > 0 || throw(ArgumentError("scale ``γ`` should be positive"))
+        #γ > 0 || throw(ArgumentError("scale ``γ`` should be positive"))
         return new{xT,yT}(x₀,γ,h)
     end
 end
@@ -57,13 +59,14 @@ function evaluate(lt::Cauchy, x)
     dnm = lt.γ * (1 + scaled^2)
     return inv(dnm) * lt.h
 end
+_lsparenttype(::Type{<:Cauchy}) = Cauchy
 
 struct RaisedCosine{xT,yT} <: LineShape{xT,yT}
     μ::xT
     s::xT
     h::yT
     function RaisedCosine(μ::xT, s::xT, h::yT) where {xT,yT}
-        s > 0 || throw(ArgumentError("scale ``s`` should be positive"))
+        #s > 0 || throw(ArgumentError("scale ``s`` should be positive"))
         return new{xT,yT}(μ,s,h)
     end
 end
@@ -74,4 +77,89 @@ function evaluate(rc::RaisedCosine, x)
     scaled = (x - rc.μ) / rc.s
     hvc = 1 + cospi(scaled)
     return hvc * rc.h
+end
+_lsparenttype(::Type{<:RaisedCosine}) = RaisedCosine
+
+############
+##
+##  FITTING PROBLEMS
+##
+############
+
+export FittingProblem, FittingSolution
+export build_objective, solve
+
+# define the fitting problem
+# make a solution struct for keeping the solution and
+# define functions for showing the fitting statistics of the solution
+# important ones are residues, sum square error, degrees of freedom, mse (rmsd)
+
+"""
+    FittingProblem
+
+Defines a problem for subsequent fitting using a solve function.
+"""
+struct FittingProblem{LS<:LineShape,iP,xT,yT}
+    targ_ls::Type{LS}
+    init_params::iP
+    xdata::xT
+    ydata::yT
+    function FittingProblem(ls::Type{<:LineShape}, ipars, xdata, ydata)
+        isbitstype(ls) || error("lineshape is not bits type")
+        rem(length(ipars), _nparams(ls)) == 0 || error("improper params vector")
+        length(xdata) == length(ydata) || DimensionMismatch("data don't have the same dimensions: got $(length(xdata)) and $(length(ydata))")
+        return new{ls,typeof(ipars),
+            typeof(xdata),typeof(ydata)}(ls, ipars, xdata, ydata)
+    end
+end
+
+## TODO: define the gradient for least squares objective
+
+"""
+    FittingSolution
+
+Represents a solution to a `FittingProblem`. Points to the original 
+`FittingProblem` solved along with the solution object from `Optim`.
+"""
+struct FittingSolution{fP<:FittingProblem,sT}
+    problem::fP
+    optimized::sT
+end
+
+"""
+    solve(::FittingProblem)
+
+Solve a `FittingProblem` and return a `FittingSolution`
+"""
+function solve end
+
+function _evaluator(LS, params)
+    # throw an error if the length of params in the compound is not divisible by the
+    # number of params in a single lineshape
+    M = _nparams(LS); LS_ = _lsparenttype(LS)
+    return function _tp(x)
+        partitioned = Iterators.partition(params, M)
+        return sum(partitioned) do part
+            evaluate(LS_(part...), x)
+        end
+    end
+end
+
+"""
+    build_objective(::FittingProblem)
+
+Return the least squares objective function to be minimized for the
+given `FittingProblem`.
+"""
+function build_objective(prob::FittingProblem{LS}) where LS
+    (; xdata, ydata) = prob
+    return function _obj(params)
+        # get y estimates from xdata
+        # get the sum of squares of residuals
+        evtor_ = _evaluator(LS, params)
+        return sum(zip(xdata, ydata)) do (x, y)
+            Δ = y - evtor_(x)
+            abs2(Δ)
+        end
+    end
 end
