61373917fba939a1ad0e9baacfe29cf9afc9e81d
Bon Leif Amalla <bonleifamalla@gmail.com>
2024-07-06 10:53:42 +0900

    Allow compat with Enzyme/ForwardDiff

--- src/lineshapes.jl
+++ src/lineshapes.jl
@@ -1,6 +1,7 @@
 # lineshapes.jl --- basic lineshapes for testing functionality and fitting
 
 export LineShape, Gaussian, Cauchy, RaisedCosine
+export CompositeShape
 export evaluate, lscenter, scale
 
 abstract type LineShape{xT,yT} end
@@ -12,8 +13,6 @@ Evaluate the line shape `ls` at the point `x`. Supported line shapes as of writi
 + `Gaussian`
 + `Lorentzian`
 + `RaisedCosine`
-
-
 """
 function evaluate end
 
@@ -21,64 +20,96 @@ function lscenter end
 function scale end
 
 _nparams(::T) where T<:LineShape = _nparams(T)
+_nparams(T::Type{<:LineShape}) = fieldcount(T)
+_outtype(::T) where T<:LineShape = _outtype(T)
 function _lsparenttype(::LineShape) end
 
 struct Gaussian{xT,yT} <: LineShape{xT,yT}
     μ::xT
     σ::xT
     h::yT
-    function Gaussian(μ::xT, σ::xT, h::yT) where {xT,yT}
+    function Gaussian{xT,yT}(μ::xT, σ::xT, h::yT) where {xT,yT}
         σ > 0 || throw(ArgumentError("scale ``σ`` should be positive"))
         return new{xT,yT}(μ,σ,h)
     end
 end
+function Gaussian(μ::Number, σ::Number, h::hT) where hT <: Number
+    μ_, σ_ = promote(μ, σ); cT = typeof(μ_)
+    return Gaussian{cT,hT}(μ_, σ_, h)
+end
 lscenter(gs::Gaussian) = gs.μ
 scale(gs::Gaussian) = gs.σ
-_nparams(::Type{<:Gaussian}) = 3
 function evaluate(gs::Gaussian, x)
     scaled = (x - gs.μ) / gs.σ
-    expd = exp(scaled^2 * -1//2)
+    expd = exp(scaled^2 * -0.5)
     return expd * gs.h / gs.σ
 end
-_lsparenttype(::Type{<:Gaussian}) = Gaussian
+_outtype(::Type{Gaussian{xT,yT}}) where {xT,yT} = yT
 
 struct Cauchy{xT,yT} <: LineShape{xT,yT}
     x₀::xT
     γ::xT
     h::yT
-    function Cauchy(x₀::xT, γ::xT, h::yT) where {xT,yT}
+    function Cauchy{xT,yT}(x₀::xT, γ::xT, h::yT) where {xT,yT}
         γ > 0 || throw(ArgumentError("scale ``γ`` should be positive"))
         return new{xT,yT}(x₀,γ,h)
     end
 end
+function Cauchy(x₀::Number, γ::Number, h::hT) where hT<:Number
+    x₀_, γ_ = promote(x₀, γ); cT = typeof(γ_)
+    return Cauchy{cT,hT}(x₀_, γ_, h)
+end
 lscenter(lt::Cauchy) = lt.x₀
 scale(lt::Cauchy) = lt.γ
-_nparams(::Type{<:Cauchy}) = 3
 function evaluate(lt::Cauchy, x)
     scaled = (x - lt.x₀) / lt.γ
     dnm = lt.γ * (1 + scaled^2)
     return inv(dnm) * lt.h
 end
-_lsparenttype(::Type{<:Cauchy}) = Cauchy
+_outtype(::Type{Cauchy{xT,yT}}) where {xT,yT} = yT
 
 struct RaisedCosine{xT,yT} <: LineShape{xT,yT}
     μ::xT
     s::xT
     h::yT
-    function RaisedCosine(μ::xT, s::xT, h::yT) where {xT,yT}
+    function RaisedCosine{xT,yT}(μ::xT, s::xT, h::yT) where {xT,yT}
         s > 0 || throw(ArgumentError("scale ``s`` should be positive"))
         return new{xT,yT}(μ,s,h)
     end
 end
+function RaisedCosine(μ::Number, s::Number, h::hT) where hT<:Number
+    μ_, s_ = promote(μ, s); cT = typeof(s_)
+    return RaisedCosine{cT,hT}(μ_, s_, h)
+end
 lscenter(rc::RaisedCosine) = rc.μ
 scale(rc::RaisedCosine) = rc.s
-_nparams(::Type{<:RaisedCosine}) = 3
 function evaluate(rc::RaisedCosine, x)
     scaled = (x - rc.μ) / rc.s
     hvc = 1 + cospi(scaled)
     return hvc * rc.h
 end
-_lsparenttype(::Type{<:RaisedCosine}) = RaisedCosine
+_outtype(::Type{RaisedCosine{xT,yT}}) where {xT,yT} = yT
+
+using StructArrays
+"""
+    CompositeShape{base<:LineShape}
+"""
+struct CompositeShape{T<:LineShape,A}
+    tparams::A
+end
+function CompositeShape(T::Type{<:LineShape}, N::Int)
+    m = _nparams(T); filler = zeros(m, N); A = typeof(filler)
+    return CompositeShape{T,A}(filler)
+end
+function CompositeShape(T::Type{<:LineShape}, tparams::AbstractVecOrMat)
+    return CompositeShape{T,typeof(tparams)}(tparams)
+end
+function evaluate(cs::CompositeShape{T}, x::Number) where T
+    # create a structvec
+    strvec = StructVector{T}(cs.tparams, dims=1)
+    xeval(str) = evaluate(str, x)
+    return sum(xeval.(strvec))
+end
 
 ############
 ##
@@ -100,7 +131,6 @@ export build_objective
 Defines a problem for subsequent fitting using a solve function.
 """
 struct FittingProblem{LS<:LineShape,iP,xT,yT}
-    targ_ls::Type{LS}
     init_params::iP
     xdata::xT
     ydata::yT
@@ -109,7 +139,7 @@ struct FittingProblem{LS<:LineShape,iP,xT,yT}
         rem(length(ipars), _nparams(ls)) == 0 || error("improper params vector")
         length(xdata) == length(ydata) || DimensionMismatch("data don't have the same dimensions: got $(length(xdata)) and $(length(ydata))")
         return new{ls,typeof(ipars),
-            typeof(xdata),typeof(ydata)}(ls, ipars, xdata, ydata)
+            typeof(xdata),typeof(ydata)}(ipars, xdata, ydata)
     end
 end
 
@@ -133,18 +163,6 @@ Solve a `FittingProblem` and return a `FittingSolution`
 """
 function solve end
 
-function _evaluator(LS, params)
-    # throw an error if the length of params in the compound is not divisible by the
-    # number of params in a single lineshape
-    M = _nparams(LS); LS_ = _lsparenttype(LS)
-    partitioned = Iterators.partition(params, M)
-    return function _tp(x)
-        return sum(partitioned) do part
-            evaluate(LS_(part...), x)
-        end
-    end
-end
-
 """
     build_objective(::FittingProblem)
 
@@ -155,11 +173,11 @@ function build_objective(prob::FittingProblem{LS}) where LS
     (; xdata, ydata) = prob
     return function _obj(params)
         # get y estimates from xdata
-        # get the sum of squares of residuals
-        evtor_ = _evaluator(LS, params)
-        return sum(zip(xdata, ydata)) do (x, y)
-            Δ = y - evtor_(x)
-            abs2(Δ)
-        end
+        # get the mean of squares of residuals
+        cs = CompositeShape(LS, reshape(params, _nparams(LS), :))
+        l = length(ydata)
+        cseval(x) = evaluate(cs, x)
+        sds = @. abs2(ydata - cseval(xdata))
+        return sum(sds) / l
     end
 end
